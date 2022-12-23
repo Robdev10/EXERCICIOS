a523f0e2f5ce4f96bf7ba2867709390659c0565d
Robdev10 <robsondev10@gmail.com>
2022-12-23 09:41:40 -0300

    estudandoangular1

--- testeangular/src/app/app.component.ts
+++ testeangular/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  username = 'samuel';
+
+  userData = {
+    email: 'robsondev10@gmail.com',
+    role: 'Supervisor',
+  };
   title = 'testeangular';
 }
--- testeangular/src/app/components/first-component/first-component.component.ts
+++ testeangular/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.scss']
+  styleUrls: ['./first-component.component.scss'],
 })
 export class FirstComponentComponent {
+  name: string = 'robson';
+  idade: number = 39;
+  job = 'programador';
 
+  constructor() {}
 }
