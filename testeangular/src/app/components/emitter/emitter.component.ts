f98050117fabadcf21318d59ba8603de3779ad66
Robdev10 <robsondev10@gmail.com>
2022-12-30 02:41:09 -0300

    29-12-22

--- testeangular/src/app/components/emitter/emitter.component.ts
+++ testeangular/src/app/components/emitter/emitter.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { MyserviceService } from 'src/app/services/myservice.service';
 
 @Component({
   selector: 'app-emitter',
   templateUrl: './emitter.component.html',
   styleUrls: ['./emitter.component.scss'],
+  providers: [MyserviceService],
 })
 export class EmitterComponent {
-  @Input() message: string;
-  constructor() {}
+  message: string;
+  constructor(private myserviceService: MyserviceService) {
+    this.message = this.myserviceService.getMessage();
+  }
 
   mynumber: number = 0;
 
   onchangenumber() {
     this.mynumber = Math.floor(Math.random() * 10);
+  }
+
+  showMessage() {
+    alert(this.message);
   }
 }
